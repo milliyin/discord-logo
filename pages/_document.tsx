63544bc6c3f9946992aec174855004894aa6f951
M.Illiyin <55222179+milliyin@users.noreply.github.com>
2024-07-24 02:05:08 +0500

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,19 +8,19 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="See pictures from Next.js Conf and the After Party."
+            content=""
           />
-          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
+          <meta property="og:site_name" content="" />
           <meta
             property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content=""
           />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
+          <meta property="og:title" content="" />
+          <meta name="twitter:card" content="" />
+          <meta name="twitter:title" content="" />
           <meta
             name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content=""
           />
         </Head>
         <body className="bg-black antialiased">
