93fd73173a0e84e63dde3fc4fa16b92daa1e9552
M.Illiyin <55222179+milliyin@users.noreply.github.com>
2024-07-30 15:31:58 +0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,11 +66,9 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
               IlliyinDesigns
             </h1>
             <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
-
              Welcome to IlliyinDesigns, your go-to for all Discord graphic needs! Enhance your Discord account and server with custom profile pictures, 
               animated banners, Discord avatar banners, and unique avatars. Our services make your Discord profile and server stand out. <br>
-               Transform your Discord profile and server today with our stunning graphics! <br><br>
-            I am working professionally as designer for 4+ years & specialized in graphic design field to create any design in Adobe Photoshop, Adobe Illustrator, Adobe After effects.
+               Transform your Discord profile and server today with our stunning graphics!
             </p>
             <a
               className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
