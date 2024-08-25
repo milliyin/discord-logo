3f7fa92d1a2ff0aa98bbd43136fffc31bceebc4a
M.Illiyin <55222179+milliyin@users.noreply.github.com>
2024-08-25 14:10:53 +0500

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,14 +6,14 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <meta
+{/*           <meta
             name="description"
             content=""
           />
-          <meta property="og:site_name" content="" />
+          <meta property="og:site_name" content="discord logo & discord banner" />
           <meta
             property="og:description"
-            content=""
+            content="illiyinDesigns.com is a company that sells graphic design services for Discord. They offer a variety of services, including profile pictures, banners, and avatars."
           />
           <meta property="og:title" content="" />
           <meta name="twitter:card" content="" />
@@ -21,7 +21,14 @@ class MyDocument extends Document {
           <meta
             name="twitter:description"
             content=""
-          />
+          /> */}
+
+<meta name="description" content="Create stunning Discord logos in seconds with our easy-to-use logo maker. Customize colors, fonts, and designs to match your brand. Download high-resolution logos for free." />
+<meta property="og:title" content="Discord Logo Maker" />
+<meta property="og:description" content="Design your own unique Discord logo online. Choose from a variety of templates and customize them to your liking. Download your logo in various formats." />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:title" content="Discord Logo Maker" />
+<meta name="twitter:description" content="Create professional Discord logos without any design experience. Our intuitive tool makes it simple to design and download your custom logo." />
         </Head>
         <body className="bg-black antialiased">
           <Main />
